2ffb77ba3314888177f89485fadd3cc4a6d14548
NOlbert <i17026@hb.dhbw-stuttgart.de>
2018-12-11 10:38:12 +0100

    Refactoring

--- TuringSimu/TuringSimu/Common/Header/BaseComparable.hpp
+++ TuringSimu/TuringSimu/Common/Header/BaseComparable.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #ifndef TM_BASECOMPARABLE
 #define TM_BASECOMPARABLE
+/**
+ * \brief An abstract class which defines operators for comparisons.
+ * \tparam T The concrete type of the implementing class. 
+ */
 template<class T> class BaseComparable
 {
 	public:
