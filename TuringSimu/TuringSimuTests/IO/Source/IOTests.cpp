7972b067de2dab1dda2831cccf0ca7355a030729
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2018-12-14 21:45:49 +0100

    Found the holy relative path

--- TuringSimu/TuringSimuTests/IO/Source/IOTests.cpp
+++ TuringSimu/TuringSimuTests/IO/Source/IOTests.cpp
@@ -5,8 +5,10 @@
 using namespace ts_common;
 using namespace boost::unit_test;
 BOOST_AUTO_TEST_CASE(TestGetTuringMachineDefinitionFromFile) {
-	//filepath is wrong so have an absolute one
-	auto tmd = ts_io::DiskIO::GetTuringMachineDefinitionFromFile("C:/Users/SBG/source/repos/Kraken/TuringSimu/TuringSimu/TuringSimuTests/IO/TestFiles/parseTest1.csv");
+	//working directory for VisualStudio is the directory in which the .vcxproj lies. So your relative path has
+	//to start there
+
+	auto tmd = ts_io::DiskIO::GetTuringMachineDefinitionFromFile("./IO/TestFiles/parseTest1.csv");
 
 	BOOST_REQUIRE(tmd.type == DTM);
 
